42031a0abff11961880d132c2267b3c3905f1f58
Sq00ky <44957111+Sq00ky@users.noreply.github.com>
2021-08-29 17:57:42 -0400

    Update agent.cpp

--- agent.cpp
+++ agent.cpp
@@ -1,9 +1,12 @@
-using namespace std;
+#include <stdio.h>
+#include <Windows.h>
+#include <ctime>
 
 /*
   This agent's role is to stay active in order to keep the credentials in memory.
 */
 
+using namespace std;
 int main()
 {
   while(1) {
