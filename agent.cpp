ff876df77c98cb6a5cae1cfbe81eb852489e9cfb
Horshark <21056390+horshark@users.noreply.github.com>
2021-08-29 23:54:48 +0200

    Create agent.cpp

---
+++ agent.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+
+/*
+  This agent's role is to stay active in order to keep the credentials in memory.
+*/
+
+int main()
+{
+  while(1) {
+    sleep(3600);   
+    return 0;
+  }
+}
